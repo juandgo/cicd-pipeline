c4c20486be57f24643630dddb470cd1260f52214
juanda <juandavidgo1997@gmail.com>
2026-01-13 13:07:05 -0500

    substract function added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,13 @@
-from src.main import add
+from src.main import add, substract
 
 def test_add_function():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(-5, -5) == -10
+
+def test_substract_function():
+    assert substract(5, 3) == 3
+    assert substract(0, 0) == 0
+    assert substract(-1, -1) == 0
+    assert substract(-5, -3) == -2
