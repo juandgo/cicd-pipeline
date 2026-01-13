879c8ed076fc212eb15b88595858b9d535404df4
juanda <juandavidgo1997@gmail.com>
2026-01-13 13:09:02 -0500

    substract function fixed

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ def test_add_function():
     assert add(-5, -5) == -10
 
 def test_substract_function():
-    assert substract(5, 3) == 3
+    assert substract(5, 3) == 2
     assert substract(0, 0) == 0
     assert substract(-1, -1) == 0
     assert substract(-5, -3) == -2
